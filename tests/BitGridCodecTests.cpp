0397802aec7d0ba731f6b77f5a9676e28e0af1c8
Haroon Awan <haroonawan2347@gmail.com>
2026-09-19 15:08:29 -0400

    got testing to work, uses Ctest

--- tests/BitGridCodecTests.cpp
+++ tests/BitGridCodecTests.cpp
@@ -1,8 +1,29 @@
 #include "BitGridCodec.h"
 
+#include <iostream>
+#include <string>
+#include <variant>
+
+using namespace std;
+
+int failures = 0;
+
+void expect(bool condition, const string &testName)
+{
+    if (!condition)
+    {
+        cerr << "FAIL: " << testName << '\n';
+        failures++;
+    }
+}
+
 int main()
 {
     BitGridCodec codec;
 
-    // tests to come
+    CodecResult encoded = codec.encode("Hello");
+    expect(holds_alternative<string>(encoded), "encode Hello succeeds");
+
+    cout << "All tests passed\n";
+    return 0;
 }
